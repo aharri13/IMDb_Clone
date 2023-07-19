fabba71a0a9b13f66dc106d7a5d5661ac2bf9145
aharri13 <60202350+aharri13@users.noreply.github.com>
2023-07-18 17:17:59 -0700

    Updates

---
+++ precache-manifest.056fa84b4633374ec49bbdda8f885b0a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "439b6ca229deb523eca1",
+    "url": "/IMDb_Clone/static/css/main.3091b004.chunk.css"
+  },
+  {
+    "revision": "439b6ca229deb523eca1",
+    "url": "/IMDb_Clone/static/js/main.439b6ca2.chunk.js"
+  },
+  {
+    "revision": "765a2e4e36b95969ee8f",
+    "url": "/IMDb_Clone/static/css/1.9923ac58.chunk.css"
+  },
+  {
+    "revision": "765a2e4e36b95969ee8f",
+    "url": "/IMDb_Clone/static/js/1.765a2e4e.chunk.js"
+  },
+  {
+    "revision": "573ee8e336fda04e12a7",
+    "url": "/IMDb_Clone/static/js/runtime~main.573ee8e3.js"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/IMDb_Clone/static/media/revicons.2feb69cc.eot"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/IMDb_Clone/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/IMDb_Clone/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "1ae0d552bfd8292af7e06b39149e6e38",
+    "url": "/IMDb_Clone/index.html"
+  }
+];
